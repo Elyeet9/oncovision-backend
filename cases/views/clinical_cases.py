468a5e2f883b5029a39f6dda84f824da86fd9bff
Elyeet9 <goyasleonardo@gmail.com>
2025-06-18 04:04:16 -0500

    Clinical case detail and image preview done!

--- cases/views/clinical_cases.py
+++ cases/views/clinical_cases.py
@@ -108,3 +108,104 @@ class ClinicalCaseCreateView(APIView):
                 {"error": str(e)},
                 status=status.HTTP_500_INTERNAL_SERVER_ERROR
             )
+
+
+class ClinicalCaseViewSet(APIView):
+    """
+    API view to retrieve details of a specific clinical case by its ID.
+    """
+    def get(self, request, *args, **kwargs):
+        pk = kwargs['pk']
+        if not pk:
+            return Response(
+                {"error": "Clinical case ID is required."},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        try:
+            clinical_case = ClinicalCase.objects.get(id=pk)
+            response_data = {
+                'id': clinical_case.id,
+                'description': clinical_case.description,
+                'patient_id': clinical_case.patient.id_number if clinical_case.patient else '-',
+                'clinical_history': clinical_case.patient.clinical_history if clinical_case.patient and clinical_case.patient.clinical_history else '-',
+            }
+            # Get medical images associated with the clinical case
+            medical_images = MedicalImaging.objects.filter(clinical_case=clinical_case)
+            for medical_image in medical_images:
+                # Check for lung nodules if they exists
+                lung_nodules = LungNodule.objects.filter(medical_imaging=medical_image)
+                nodule_data = []
+                for lung_nodule in lung_nodules:
+                    nodule_data.append({
+                        'id': lung_nodule.id,
+                        'medical_imaging_id': lung_nodule.medical_imaging.id,
+                        'malignancy_type': lung_nodule.malignancy_type,
+                        'x_position': lung_nodule.x_position,
+                        'y_position': lung_nodule.y_position,
+                        'width': lung_nodule.width,
+                        'height': lung_nodule.height,
+                    })
+                
+                # Add the medical image data to the response
+            response_data['medical_images'] = (
+                {
+                    'id': img.id,
+                    'state': img.state,
+                    'full_image': img.full_image.url if img.full_image else None,
+                    'processed_image': img.processed_image.url if img.processed_image else None,
+                    'lung_nodules': nodule_data
+                } for img in medical_images
+            )
+
+            return Response(response_data, status=status.HTTP_200_OK)
+        # Handle case where clinical case does not exist
+        except ClinicalCase.DoesNotExist:
+            return Response(
+                {"error": "Clinical case not found."},
+                status=status.HTTP_404_NOT_FOUND
+            )
+
+
+class ClinicalCaseUploadImagesView(APIView):
+    """
+    API view to upload images for a clinical case.
+    """
+
+    def post(self, request, *args, **kwargs):
+        # This method should handle the upload of images for a clinical case
+        data = request.data
+
+        # Get clinical_case_id
+        clinical_case_id = data.get('case_id', None)
+        if not clinical_case_id:
+            return Response(
+                {"error": "Clinical case ID is required."},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        try:
+            clinical_case = ClinicalCase.objects.get(id=clinical_case_id)
+            # Process the uploaded images and associate them with the clinical case
+            images = data.getlist('files', [])
+            if not images:
+                return Response(
+                    {"error": "No images provided."},
+                    status=status.HTTP_400_BAD_REQUEST
+                )
+            for image in images:
+                # Create a new MedicalImaging instance for each image
+                medical_image = MedicalImaging.objects.create(
+                    clinical_case=clinical_case,
+                    full_image=image,
+                    state='preview'  # Set the initial state to 'uploaded'
+                )
+                medical_image.save()
+            return Response(
+                {"message": "Images uploaded successfully", "clinical_case_id": clinical_case.id},
+                status=status.HTTP_201_CREATED
+            )
+        except ClinicalCase.DoesNotExist:
+            return Response(
+                {"error": "Clinical case not found."},
+                status=status.HTTP_404_NOT_FOUND
+            )
